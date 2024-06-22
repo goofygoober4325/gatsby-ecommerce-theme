51209c882f4955867e20c52a2711dae643b64db2
goofygoober4325 <myselfsahanabhat@gmail.com>
2024-06-23 04:37:50 +0530

    Add test error button for Sentry

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,6 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://ee0e57855a28a3bce6bca6f9184afee0@o4507477510193152.ingest.us.sentry.io/4507477514321920",
+  dsn: "https://de92adaf09fda3030f4cfa1caf0d0928@o4507477510193152.ingest.us.sentry.io/4507478090645504",
   tracesSampleRate: 1.0,
 });
